3b4730cc216c1b0c20a9a8f7026092f7f9746e03
duanquanyong <duanquanyong@bianfeng.com>
2019-12-03 16:43:14 +0800

    添加配置文件

--- tender.js
+++ tender.js
@@ -1,13 +1,17 @@
-Tender.server("git.imeete.com")(function (config) {
-    config.name = 1
-    config.b = 2
-    config.c = "xxx"
+Tender.server("git.imeete.com")(function (server) {
+    server.name = 1
+    server.b = 2
+    server.c = "xxx"
     console.log("这份", this)
 })
 
-Tender.server("git.imeete.com")(function (config) {
-    config.name = 1
-    config.b = 2
-    config.c = "xxx"
+Tender.server("git.imeete.com")(function (server) {
+    server.name = 1
+    server.b = 2
+    server.c = "xxx"
     console.log("这份", this)
+
+    server.tls()(function (tls) {
+
+    })
 })
